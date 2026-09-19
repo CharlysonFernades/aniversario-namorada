2b37f22c842027828d003258ae97d95614881415
Charlyson Fernandes <pontescharlyson@gmail.com>
2026-09-19 11:53:59 -0300

    Implement foundation: js/music-player.js

---
+++ js/music-player.js
@@ -0,0 +1,4 @@
+/**
+ * Fundação do player. Nenhuma música real é carregada nesta etapa.
+ */
+(function(){window.musicPlayer={ready:false,source:null,play:function(){return Promise.resolve(false)},pause:function(){return false}}})();
