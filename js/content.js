fceca00f2f0aefa3254187ea3a32381da13e46ce
Charlyson Fernandes <pontescharlyson@gmail.com>
2026-09-20 12:32:10 -0300

    Add files via upload

--- js/content.js
+++ js/content.js
@@ -17,9 +17,31 @@ window.siteContent={
   ],
   photos:{
     narrative:{
-      story:[],
+      story:[
+        {image:"assets/fotos/imagem23.webp",alt:"NOSSA PRIMEIRA FOTO JUNTOS.",title:"O começo de tudo.",caption:"Aqui fora onde nós tiramos a nossa primeira foto juntos, mal esperando o futuro incrível que nos aguardava."
+        }
+      ],
       memories:[
-        {image:"",alt:"[ALT DA FOTO 01]",title:"[MOMENTO 01]",caption:"[LEGENDA CURTA]"},
+        {image:"assets/fotos/foto01.jpg",alt:"Você deitada no meu ombro.",title:"VOCÊ APOIADA NO MEU OMBRO",caption:"Aqui você já demonstrava um amor e confiança por nós."},
+        {image:"",alt:"[ALT DA FOTO 02]",title:"[MOMENTO 02]",caption:"[LEGENDA CURTA]"},
+        {image:"",alt:"[ALT DA FOTO 02]",title:"[MOMENTO 02]",caption:"[LEGENDA CURTA]"},
+        {image:"",alt:"[ALT DA FOTO 02]",title:"[MOMENTO 02]",caption:"[LEGENDA CURTA]"},
+        {image:"",alt:"[ALT DA FOTO 02]",title:"[MOMENTO 02]",caption:"[LEGENDA CURTA]"},
+        {image:"",alt:"[ALT DA FOTO 02]",title:"[MOMENTO 02]",caption:"[LEGENDA CURTA]"},
+        {image:"",alt:"[ALT DA FOTO 02]",title:"[MOMENTO 02]",caption:"[LEGENDA CURTA]"},
+        {image:"",alt:"[ALT DA FOTO 02]",title:"[MOMENTO 02]",caption:"[LEGENDA CURTA]"},
+        {image:"",alt:"[ALT DA FOTO 02]",title:"[MOMENTO 02]",caption:"[LEGENDA CURTA]"},
+        {image:"",alt:"[ALT DA FOTO 02]",title:"[MOMENTO 02]",caption:"[LEGENDA CURTA]"},
+        {image:"",alt:"[ALT DA FOTO 02]",title:"[MOMENTO 02]",caption:"[LEGENDA CURTA]"},
+        {image:"",alt:"[ALT DA FOTO 02]",title:"[MOMENTO 02]",caption:"[LEGENDA CURTA]"},
+        {image:"",alt:"[ALT DA FOTO 02]",title:"[MOMENTO 02]",caption:"[LEGENDA CURTA]"},
+        {image:"",alt:"[ALT DA FOTO 02]",title:"[MOMENTO 02]",caption:"[LEGENDA CURTA]"},
+        {image:"",alt:"[ALT DA FOTO 02]",title:"[MOMENTO 02]",caption:"[LEGENDA CURTA]"},
+        {image:"",alt:"[ALT DA FOTO 02]",title:"[MOMENTO 02]",caption:"[LEGENDA CURTA]"},
+        {image:"",alt:"[ALT DA FOTO 02]",title:"[MOMENTO 02]",caption:"[LEGENDA CURTA]"},
+        {image:"",alt:"[ALT DA FOTO 02]",title:"[MOMENTO 02]",caption:"[LEGENDA CURTA]"},
+        {image:"",alt:"[ALT DA FOTO 02]",title:"[MOMENTO 02]",caption:"[LEGENDA CURTA]"},
+        {image:"",alt:"[ALT DA FOTO 02]",title:"[MOMENTO 02]",caption:"[LEGENDA CURTA]"},
         {image:"",alt:"[ALT DA FOTO 02]",title:"[MOMENTO 02]",caption:"[LEGENDA CURTA]"},
         {image:"",alt:"[ALT DA FOTO 03]",title:"[MOMENTO 03]",caption:"[LEGENDA CURTA]"}
       ]
