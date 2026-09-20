47f5e7dbbbaf96a0b46e26c793c998f88fb93dab
Charlyson Fernandes <pontescharlyson@gmail.com>
2026-09-20 10:57:54 -0300

    Formato da capa da musica alterado de jpg pra png

--- js/content.js
+++ js/content.js
@@ -10,10 +10,10 @@ window.siteContent={
   story:{title:"[TÍTULO DA HISTÓRIA]",text:"[ESPAÇO PARA UMA MENSAGEM OU INTRODUÇÃO DA HISTÓRIA]"},
   memories:{title:"[TÍTULO DAS MEMÓRIAS]",text:"[ESPAÇO RESERVADO PARA MEMÓRIAS, FOTOS E MOMENTOS]"},
   musicas:[
-    {titulo:"Música 01",artista:"Artista",arquivo:"assets/musicas/musica01.mp3",capa:"assets/fotos/musicas/musica01.jpg",altCapa:"Capa da Música 01"},
-    {titulo:"Música 02",artista:"Artista",arquivo:"assets/musicas/musica02.mp3",capa:"assets/fotos/musicas/musica02.jpg",altCapa:"Capa da Música 02"},
-    {titulo:"Música 03",artista:"Artista",arquivo:"assets/musicas/musica03.mp3",capa:"assets/fotos/musicas/musica03.jpg",altCapa:"Capa da Música 03"},
-    {titulo:"Música 04",artista:"Artista",arquivo:"assets/musicas/musica04.mp3",capa:"assets/fotos/musicas/musica04.jpg",altCapa:"Capa da Música 04"}
+    {titulo:"Música 01",artista:"Artista",arquivo:"assets/musicas/musica01.mp3",capa:"assets/fotos/musicas/musica01.png",altCapa:"Capa da Música 01"},
+    {titulo:"Música 02",artista:"Artista",arquivo:"assets/musicas/musica02.mp3",capa:"assets/fotos/musicas/musica02.png",altCapa:"Capa da Música 02"},
+    {titulo:"Música 03",artista:"Artista",arquivo:"assets/musicas/musica03.mp3",capa:"assets/fotos/musicas/musica03.png",altCapa:"Capa da Música 03"},
+    {titulo:"Música 04",artista:"Artista",arquivo:"assets/musicas/musica04.mp3",capa:"assets/fotos/musicas/musica04.png",altCapa:"Capa da Música 04"}
   ],
   photos:{
     narrative:{
