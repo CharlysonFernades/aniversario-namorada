b820cfe709f5da30b485a97ac4b7db452d9b982c
Charlyson Fernandes <pontescharlyson@gmail.com>
2026-09-23 16:50:29 -0300

    Update content.js
    foi adicionado uma parte na "Mensagem secreta", uma frase que vai direcionar a namorada olhar para trás e ver as fotos da "festa de 18 anos".

--- js/content.js
+++ js/content.js
@@ -35,7 +35,7 @@ window.siteContent={
     title:"AQUI ESTÁ ESCONDIDO UM SEGREDO...",
     intro:"É uma coisa que você vai querer saber...",
     actionLabel:"REVELAR SEGREDO",
-    message:"Você é a dona do sorriso mais lindo desse planeta, é a mulher mais maravilhosa, mais forte que eu já conheci, eu quero viver um futuro lindo com você. EU TE AMO MEU AMOR. e aqui o segredo: Eu gosto de sentir o cheiro dos seus peidos..."
+    message:"Você é a dona do sorriso mais lindo desse planeta, é a mulher mais maravilhosa, mais forte que eu já conheci, eu quero viver um futuro lindo com você. EU TE AMO MEU AMOR. e aqui o segredo: Eu gosto de sentir o cheiro dos seus peidos...(brincadeira, olha pra trás aí.)"
   },
   gift:{
     title:"Uma carta para você",
